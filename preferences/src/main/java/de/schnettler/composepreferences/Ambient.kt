2de6310bd8bc03a7339b283558b7020af3a57c57
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-08-29 15:18:46 +0200

    Preferences: Provide Composable 'ProvidePreferences' which wraps the content with a PreferenceAmbient

--- preferences/src/main/java/de/schnettler/composepreferences/Ambient.kt
+++ preferences/src/main/java/de/schnettler/composepreferences/Ambient.kt
@@ -1,8 +1,20 @@
 package de.schnettler.composepreferences
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidableAmbient
+import androidx.compose.runtime.Providers
 import androidx.compose.runtime.ambientOf
 import com.tfcporciuncula.flow.FlowSharedPreferences
 
 val PreferenceAmbient: ProvidableAmbient<FlowSharedPreferences> =
     ambientOf { error("No preferences found") }
+
+@Composable
+fun ProvidePreferences(
+    sharedPreferences: FlowSharedPreferences,
+    content: @Composable () -> Unit
+) {
+    Providers(PreferenceAmbient provides sharedPreferences) {
+        content()
+    }
+}
