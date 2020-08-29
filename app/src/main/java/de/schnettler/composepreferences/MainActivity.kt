10106c8d1912fe5260b6e4618533b03a84eb13cc
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-08-29 15:50:19 +0200

    Sample: Group Settings in PreferenceGroups

--- app/src/main/java/de/schnettler/composepreferences/MainActivity.kt
+++ app/src/main/java/de/schnettler/composepreferences/MainActivity.kt
@@ -50,38 +50,45 @@ fun PreferenceScreen() {
             singleLineTitle = true,
             icon = Icons.Outlined.Warning
         )
-        ListPreference(
-            title = "List Preference",
-            summary = "Select one item from a list in a dialog",
-            key = "pref_list",
-            singleLineTitle = true,
-            icon = Icons.Outlined.Warning,
-            entries = mapOf(
-                "key1" to "Item1",
-                "key2" to "Item2"
+
+
+        PreferenceGroup(title = "List Group") {
+            ListPreference(
+                title = "List Preference",
+                summary = "Select one item from a list in a dialog",
+                key = "pref_list",
+                singleLineTitle = true,
+                icon = Icons.Outlined.Warning,
+                entries = mapOf(
+                    "key1" to "Item1",
+                    "key2" to "Item2"
+                )
             )
-        )
-        MultiSelectListPreference(
-            title = "MultiSelect List Preference",
-            summary = "Select multiple items from a list in a dialog",
-            key = "pref_multi_list",
-            singleLineTitle = true,
-            icon = Icons.Outlined.Warning,
-            entries = mapOf(
-                "key1" to "Item1",
-                "key2" to "Item2"
+            MultiSelectListPreference(
+                title = "MultiSelect List Preference",
+                summary = "Select multiple items from a list in a dialog",
+                key = "pref_multi_list",
+                singleLineTitle = true,
+                icon = Icons.Outlined.Warning,
+                entries = mapOf(
+                    "key1" to "Item1",
+                    "key2" to "Item2"
+                )
             )
-        )
-        SeekBarPreference(
-            title = "Seekbar Preference",
-            summary = "Select a value on a seekbar",
-            key = "pref_seek",
-            defaultValue = 50F,
-            singleLineTitle = true,
-            icon = Icons.Outlined.Warning,
-            steps = 4,
-            valueRange = 50F..100F,
-            valueRepresentation = { value -> "$value %" }
-        )
+        }
+
+        PreferenceGroup(title = "Seekbar Group") {
+            SeekBarPreference(
+                title = "Seekbar Preference",
+                summary = "Select a value on a seekbar",
+                key = "pref_seek",
+                defaultValue = 50F,
+                singleLineTitle = true,
+                icon = Icons.Outlined.Warning,
+                steps = 4,
+                valueRange = 50F..100F,
+                valueRepresentation = { value -> "$value %" }
+            )
+        }
     }
 }
