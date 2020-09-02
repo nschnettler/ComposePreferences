6fddb47fae054723e4886347784233b691057a8d
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-09-02 19:55:52 +0200

    Update AGP 4.2 Alpha8

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha07")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha08")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0-rc")
     }
 }
