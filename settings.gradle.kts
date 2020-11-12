9b75f927dd033744712169f9ec751365286051a5
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-11-12 11:51:59 +0100

    Add RefreshVersions plugin for dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
+import de.fayard.refreshVersions.bootstrapRefreshVersions
+
 rootProject.name = "ComposePreferences"
 include(":app")
 include(":preferences")
+
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies.classpath("de.fayard.refreshVersions:refreshVersions:0.9.7")
+}
+
+bootstrapRefreshVersions()
