014cd44bfd8ecd26e97bbe17038ff7d2bdc11b04
Niklas Schnettler <niklas.schnettler@gmail.com>
2021-05-15 17:12:55 +0200

    Update AGP to 7.0.0-alpha15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
 
     defaultConfig {
         applicationId = "de.schnettler.composepreferences"
-        minSdkVersion(21)
-        targetSdkVersion(30)
+        minSdk = 21
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
     }
