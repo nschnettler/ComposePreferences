7e203adf1e2088087343ce233d0219b214662b63
Rk779 <Rahulkrishna585@gmail.com>
2021-07-11 20:18:04 +0530

    app: Update Sample application
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>

--- app/src/main/java/de/schnettler/composepreferences/Settings.kt
+++ app/src/main/java/de/schnettler/composepreferences/Settings.kt
@@ -1,10 +1,19 @@
 package de.schnettler.composepreferences
 
-import androidx.datastore.preferences.core.*
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.floatPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import de.schnettler.datastore.manager.PreferenceRequest
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
 object SwitchPrefExample : PreferenceRequest<Boolean>(booleanPreferencesKey("pref_switch"), true)
-object ListPrefExample: PreferenceRequest<String>(stringPreferencesKey("pref_list"), "")
-object MultiPrefExample: PreferenceRequest<Set<String>>(stringSetPreferencesKey("pref_multi_list"), emptySet())
-object SeekPrefExample: PreferenceRequest<Float>(floatPreferencesKey("pref_seek"), 50F)
-object DropDownPrefExample: PreferenceRequest<String>(stringPreferencesKey("pref_dropdown_list"), "")
+object ListPrefExample : PreferenceRequest<String>(stringPreferencesKey("pref_list"), "")
+object MultiPrefExample : PreferenceRequest<Set<String>>(stringSetPreferencesKey("pref_multi_list"), emptySet())
+object SeekPrefExample : PreferenceRequest<Float>(floatPreferencesKey("pref_seek"), 50F)
+object DropDownPrefExample : PreferenceRequest<String>(stringPreferencesKey("pref_dropdown_list"), "")
